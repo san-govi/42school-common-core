5e0cca20d592aea5310bb23333570b83d3437014
sgovinda <sgovinda@student.42london.com>
2025-11-30 17:07:09 +0000

    Add ft_strnstr function and test harness.

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgovinda <sgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 15:39:02 by sgovinda          #+#    #+#             */
+/*   Updated: 2025/11/30 17:02:05 by sgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (*little == '\0') 
+	{
+		return ((char *)big);
+	}
+	i = 0;
+	while (i < len && big[i] != '\0')
+	{
+		j = 0;
+		while (i + j < len && big[i + j] == little[j]) 
+		{
+				if (little[j + 1] == '\0')
+				{
+					return ((char *)(big + i));
+				}
+				j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ libft/test_strnstr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_strnstr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgovinda <sgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 17:03:32 by sgovinda          #+#    #+#             */
+/*   Updated: 2025/11/30 17:06:13 by sgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+int main()
+{
+	const char *big = "Hello, this is a simple test string.";
+	const char *little = "simple";
+	size_t len = 30;
+
+	char *result = ft_strnstr(big, little, len);
+	if (result)
+		printf("Found: %s\n", result);
+	else
+		printf("Not Found\n");
+
+	return 0;
+}
