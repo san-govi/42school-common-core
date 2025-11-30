a119658dc40cc2dad133c91f6554769b1a7b4ea5
sgovinda <sgovinda@student.42london.com>
2025-11-30 15:38:01 +0000

    Add ft_memcmp function and test harness.

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgovinda <sgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 15:26:06 by sgovinda          #+#    #+#             */
+/*   Updated: 2025/11/30 15:33:01 by sgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*p1;
+	const unsigned char	*p2;
+
+	p1 = (const unsigned char *)s1;
+	p2 = (const unsigned char *)s2;
+	while (n > 0)
+	{
+		if(*p1 != *p2)
+		{
+			return(*p1 - *p2);
+		}
+		p1++;
+		p2++;
+		n--;
+	}
+	return (0);
+}
---
+++ libft/test_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_memcmp.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgovinda <sgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 15:33:43 by sgovinda          #+#    #+#             */
+/*   Updated: 2025/11/30 15:36:49 by sgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+int	main(void)
+{
+	// returns 0
+	printf("%d\n", ft_memcmp("abc", "abc", 3));
+	// returns -99
+	printf("%d\n", ft_memcmp("ab", "abc", 3));
+	// return +99
+	printf("%d\n", ft_memcmp("abc", "ab", 3));
+	// returns -1
+	printf("%d\n", ft_memcmp("abc", "abd", 3));
+	// returns -1
+	printf("%d\n", ft_memcmp("abc", "abd", 5));
+	// returns 0
+	printf("%d\n", ft_memcmp("abc", "abc", 5));
+}
