3728fef8802f316014a2342a7e73135dd6bb5fa9
sgovinda <sgovinda@student.42london.com>
2025-12-02 16:52:33 +0000

    Add ft_lstiter binus function and test harness.

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sgovinda <sgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 20:46:37 by sgovinda          #+#    #+#             */
-/*   Updated: 2025/12/02 16:21:29 by sgovinda         ###   ########.fr       */
+/*   Updated: 2025/12/02 16:40:49 by sgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
---
+++ libft/test_lstiter.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_lstiter.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgovinda <sgovinda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 16:43:01 by sgovinda          #+#    #+#             */
+/*   Updated: 2025/12/02 16:49:26 by sgovinda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+void print_content(void *content)
+{
+    printf("%s\n", (char *)content);
+}
+
+int	main(void)
+{
+	t_list	*head;
+	char	*str;
+	char	*str2;
+	char	*str3;
+	
+	str = ft_strdup("A");
+	str2 = ft_strdup("B");
+	str3 = ft_strdup("C");
+	head = ft_lstnew(str);
+	head->next = ft_lstnew(str2); 
+	head->next->next = ft_lstnew(str3);
+	ft_lstiter(head, print_content);
+	return (0);
+}
