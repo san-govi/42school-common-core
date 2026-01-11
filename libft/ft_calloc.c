df0b4d27f2e41250a9c6984576b3b06c21269d19
sgovinda <sgovinda@student.42london.com>
2026-01-11 16:47:30 +0000

    Update calloc to check size to allocate is not greater than size_max.

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,23 +6,32 @@
 /*   By: sgovinda <sgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 18:57:53 by sgovinda          #+#    #+#             */
-/*   Updated: 2025/12/01 12:46:26 by sgovinda         ###   ########.fr       */
+/*   Updated: 2025/12/10 09:42:51 by sgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdint.h> 
 #include "libft.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 	size_t	i;
+	size_t	total_size;
 
-	ptr = malloc(nmemb * size);
+	if (size == 0 || nmemb == 0)
+	{
+		return malloc(0);
+	}
+	if (size != 0 && nmemb > SIZE_MAX / size)
+		return (NULL);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
 	if (!ptr)
 		return (NULL);
 	i = 0;
-	while (i < nmemb * size)
+	while (i < total_size)
 	{
 		((unsigned char *)ptr)[i] = 0;
 		i++;
