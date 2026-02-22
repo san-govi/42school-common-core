e80225f66a1c189fce571e6694d02bcac9d4a781
sgovinda <sgovinda@student.42london.com>
2026-02-22 01:00:05 +0000

    test: update main to include %s string specifier test cases

--- printf_tests/main.c
+++ printf_tests/main.c
@@ -6,7 +6,7 @@
 /*   By: sgovinda <sgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/21 16:31:42 by sgovinda          #+#    #+#             */
-/*   Updated: 2026/02/21 22:36:25 by sgovinda         ###   ########.fr       */
+/*   Updated: 2026/02/22 00:28:26 by sgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int	main(void)
 {
+	// Tests %c specifier and invalid specifier.
 	ft_printf("I have an %c and a %c and a %f", 'A', 'D', 'C');
+
+	// Test %s specifier and null argument.
+	ft_printf("Hello %s and hi %s. Goodbye %f.", "Sangeetha", "Daniel", 'C');
+	ft_printf("Hello %s and hi %s. Goodbye %f.", "Sangeetha");
+	
 }
